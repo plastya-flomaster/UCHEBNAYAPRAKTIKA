a904b244c95186f0afe21742d0efceb2fde5324d
Anastasia <nastyapl98@gmail.com>
2018-06-15 16:09:47 +0500

    404 + auct

---
+++ UCHEBNAYAPRAKTIKA/Controllers/AuctionsController.cs
@@ -0,0 +1,167 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UCHEBNAYAPRAKTIKA.Models;
+
+namespace UCHEBNAYAPRAKTIKA.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        private ProcurementRegEntities db = new ProcurementRegEntities();
+
+        // GET: Auctions
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+            var auctions = from a in db.Auctions
+                           select a;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                auctions = auctions.Where(a => a.AuctionName.Contains(searchString)
+                                       || a.Website.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    auctions = auctions.OrderByDescending(s => s.AuctionName);
+                    break;
+               default:  // Name ascending 
+                    auctions = auctions.OrderBy(s => s.AuctionName);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(auctions.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Auctions/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auction auction = db.Auctions.Find(id);
+            if (auction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auction);
+        }
+
+        // GET: Auctions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auctions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AuctionKey,AuctionName,Website,Deleted")] Auction auction)
+        {
+            if (ModelState.IsValid)
+            {
+                auction.Deleted = false;
+                auction.AuctionKey = Guid.NewGuid();
+                db.Auctions.Add(auction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(auction);
+        }
+
+        // GET: Auctions/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auction auction = db.Auctions.Find(id);
+            if (auction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auction);
+        }
+
+        // POST: Auctions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AuctionKey,AuctionName,Website,Deleted")] Auction auction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(auction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(auction);
+        }
+
+        // GET: Auctions/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auction auction = db.Auctions.Find(id);
+            if (auction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auction);
+        }
+
+        // POST: Auctions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Auction auction = db.Auctions.Find(id);
+            auction.Deleted = true;
+            db.Entry(auction).State = EntityState.Modified;
+            //db.Auctions.Remove(auction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UCHEBNAYAPRAKTIKA/Controllers/CitiesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UCHEBNAYAPRAKTIKA.Models;
+
+namespace UCHEBNAYAPRAKTIKA.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private ProcurementRegEntities db = new ProcurementRegEntities();
+
+        // GET: Cities
+        public ActionResult Index()
+        {
+            var cities = db.Cities.Include(c => c.Region);
+            return View(cities.ToList());
+        }
+
+        // GET: Cities/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // GET: Cities/Create
+        public ActionResult Create()
+        {
+            ViewBag.RegionKey = new SelectList(db.Regions, "RegionKey", "RegionName");
+            return View();
+        }
+
+        // POST: Cities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CityKey,RegionKey,CityName,Deleted")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                city.CityKey = Guid.NewGuid();
+                db.Cities.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RegionKey = new SelectList(db.Regions, "RegionKey", "RegionName", city.RegionKey);
+            return View(city);
+        }
+
+        // GET: Cities/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RegionKey = new SelectList(db.Regions, "RegionKey", "RegionName", city.RegionKey);
+            return View(city);
+        }
+
+        // POST: Cities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CityKey,RegionKey,CityName,Deleted")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RegionKey = new SelectList(db.Regions, "RegionKey", "RegionName", city.RegionKey);
+            return View(city);
+        }
+
+        // GET: Cities/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            City city = db.Cities.Find(id);
+            db.Cities.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
